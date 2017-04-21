247c055dd040cce7c61a11855bd56cff8687a9e4
Adam Goth <adamgoth@gmail.com>
2017-04-20 18:20:45 -0600

    Adds Firebase config

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount(){
+  const config = {
+    apiKey: 'AIzaSyBj4oGmzDynISEHtnX33Pzub9mOQXDvUJU',
+    authDomain: 'react-native-manager-243b3.firebaseapp.com',
+    databaseURL: 'https://react-native-manager-243b3.firebaseio.com',
+    projectId: 'react-native-manager-243b3',
+    storageBucket: 'react-native-manager-243b3.appspot.com',
+    messagingSenderId: '697680890499'
+    };
+
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
