b021af04b7afe13ddfb1506e7089c0fbb10d2b80
Adam Goth <adamgoth@gmail.com>
2017-05-04 18:08:43 -0600

    Fixes bug in ListItem by binding this

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -12,7 +12,7 @@ class ListItem extends Component {
     const { name } = this.props.employee;
 
     return (
-      <TouchableWithoutFeedback onPress={this.onRowPress}>
+      <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
         <View>
           <CardSection>
             <Text style={styles.titleStyle}>
